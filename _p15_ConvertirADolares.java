f1021c96c7bf6770368ef94b62e91be29fd21d07
ELdiegoGDL” <diego.garciad@uap.uaz.edu.mx>
2023-08-24 09:03:38 -0600

    listo master

--- _p15_ConvertirADolares.java
+++ _p15_ConvertirADolares.java
@@ -4,7 +4,9 @@ import java.util.Scanner;
 
 public class _p15_ConvertirADolares {
     public static void main(String[] args) {
-        
+        System.out.print("\033[H\033[2J"); System.out.flush(); // limpia la pantalla de la consola
+
+
         double pesos, dolares, cotizacion;
 
         Scanner obj =new Scanner(System.in);
